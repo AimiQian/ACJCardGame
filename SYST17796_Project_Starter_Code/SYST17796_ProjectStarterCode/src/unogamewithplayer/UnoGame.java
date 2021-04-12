da14bdda7536c4d763adec86dc13c1d9dabf7eb4
ChristianaKiervin <77470585+ChristianaKiervin@users.noreply.github.com>
2021-04-12 15:45:12 -0400

    Comments for UnoGame

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/unogamewithplayer/UnoGame.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/unogamewithplayer/UnoGame.java
@@ -10,7 +10,11 @@ import java.util.Scanner;
 
 /**
  * @author Aimi Qian
- * This class extends Game class in starter code
+ * 
+ * modified by: Christiana Kiervin & Joshua Miguel David
+ * 
+ * This class extends Game class in starter code. It contains methods which allow for the main functionality of the Uno game-play from registering players to declaring a winner.
+ * 
  */
 public class UnoGame extends Game {
     private CardPile unPlayedCP = new CardPile(108);
@@ -19,10 +23,19 @@ public class UnoGame extends Game {
     private int orderStep =1; //could be either 1 or -1
     private int currentPlayerIndex = -1;
 
+    /**
+     * Single parameter constructor to create the Uno Game
+     * 
+     * @param givenName 
+     */
     public UnoGame(String givenName){
         super(givenName);
     }
 
+    /**
+     * A method to create new UnoPlayer objects for each player in the game by retrieving user inputted names.
+     * 
+     */
     private void registerPlayer(){
         System.out.println("Uno Player Registration");
         Scanner in = new Scanner(System.in);
@@ -44,6 +57,10 @@ public class UnoGame extends Game {
         }
     }
 
+    
+    /**
+     * A method to welcome all players once they have been registered.
+     */
     private void welcomePlayer(){
         System.out.println("Welcome Players: ");
         for (Player p : this.getPlayers()){
@@ -53,20 +70,24 @@ public class UnoGame extends Game {
 
     }
 
+    /**
+     * A method to initialize the deck of uno cards. The unPlayed card pile object will receive 108 cards total.
+     * 
+     *  Normal Cards = 76:
+     *      1-9: 9 number * 4 color * 2 = 72  
+     *      0: 4 color = 4
+     * 
+     *  Functional Cards = 32:
+     *      BanCard: 4 color * 2 = 8
+     *      SwitchCard : 4 color * 2 = 8
+     *      PlusTwoCard: 4 color * 2 = 8
+     *      ChangeColorCard: 4
+     *      PlusFourCard: 4
+     * 
+     *  Total 108 cards
+     * 
+     */
     private void iniCards(){
-        /*
-            Normal Cards = 76:
-                1-9: 9 number * 4 color * 2 = 72
-                0: 4 color = 4
-            Functional Cards = 32:
-                BanCard: 4 color * 2 = 8
-                SwitchCard : 4 color * 2 = 8
-                PlusTwoCard: 4 color * 2 = 8
-                ChangeColorCard: 4
-                PlusFourCard: 4
-
-            Total 108 cards
-         */
 
         for(Color color: Color.values()){
             //generate normal cards ( 0 - 9 ) * 4 colors
@@ -97,7 +118,11 @@ public class UnoGame extends Game {
         this.unPlayedCP.shuffle();
     }
 
-    //each player get 7 cards randomly
+   
+    /**
+     * A method to deal 7 cards to each player. The methood will randomly draw cards from the unPlayed Card Pile and transfer them to the UnoPlayer's hand cardPile.
+     * 
+     */
     private void dealCards(){
         for(int i = 0; i < 7; i++){
             for(Player p : this.getPlayers()){
@@ -110,7 +135,11 @@ public class UnoGame extends Game {
         }
     }
 
-    //get next player
+    /**
+     * nextPlayer will return the Player object for the player whose turn is next.
+     * 
+     * @return the UnoPlayer whose turn is next.
+     */
     private UnoPlayer nextPlayer(){
         this.currentPlayerIndex = (this.currentPlayerIndex + this.orderStep) % this.getPlayers().size();
         if(this.currentPlayerIndex < 0){
@@ -120,11 +149,19 @@ public class UnoGame extends Game {
         return (UnoPlayer) this.getPlayers().get(this.currentPlayerIndex);
     }
 
-    //change the direction
+
+    /**
+     * A method to reverse the direction of current gameplay. It is called by special function ReverseDir cards to alter the UnoGame game-play.
+     */
     private void reverse(){
         this.orderStep = this.orderStep * -1; // orderStep can only be 1 or -1
     }
 
+    /**
+     * Given a minimum number of cards allowed in a cardPile, this method ensures the players do not run out of cards to draw from by combining the played cards with unplayed cards.
+     * 
+     * @param minimum the minimum number of cards allowed in the unPlayed Card Pile before the decks will be combined and shuffled.
+     */
     private void checkAndSupply(int minimum){
         // check if the number of card in the unplayed card pile falls below minimum amount
         // if so, add all played cards to unplayed pile and shuffle
@@ -136,7 +173,16 @@ public class UnoGame extends Game {
         }
     }
 
-    //the player will get penalty : take a specific number of cards from unplayed card pile
+    /**
+     * A method to add cards from the unPlayed deck to a player's hand.
+     * 
+     * The player will receive cards when they cannot play any of their current cards or if they are penalized by a PlusTwo or PlusFour function card.
+     * 
+     * The method will add the specified number of cards to the given player's hand cardpile.
+     * 
+     * @param player the player who will be receiving cards
+     * @param num the number of cards a player must take
+     */
     private void penalize(UnoPlayer player, int num){
         this.checkAndSupply(num); //check if there are least number of cards left
 
@@ -149,6 +195,9 @@ public class UnoGame extends Game {
 
     }
 
+    /**
+     * The play method calls upon all necessary methods to initialize a game and begins to play.
+     */
     @Override
     public void play(){
         System.out.println("Uno Game Start");
@@ -211,6 +260,10 @@ public class UnoGame extends Game {
 
     }
 
+    /**
+     * A method to declare the winner of the game.
+     * 
+     */
     public void declareWinner(){
         Player winner = this.getPlayers().get(this.currentPlayerIndex);
         System.out.println("\nWinner is " + winner.getPlayerID() + "\nGame over! Thanks for playing!");
